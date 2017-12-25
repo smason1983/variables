6a9a98c35c17edf300b926e48d0681a92f647a4a
Scott Mason <34557195+smason1983@users.noreply.github.com>
2017-12-25 13:58:06 -0500

    Add files via upload

---
+++ variables.rb
@@ -0,0 +1,12 @@
+my_string = '...you can say that again...'
+puts my_string
+puts my_string
+name = 'Anya Christina Emanuella Jenkins Harris'
+puts 'My name is ' + name + '.'
+puts 'Wow' + name
+puts 'is a really long name!'
+composer = 'Mozart'
+puts composer + ' was "da bomb" in his day.'
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.'
+my_own_var = 'just another' + 'string'
