2b8fddb959b967bdbdbaaf8cd39b8d53c2274551
Scott Mason <34557195+smason1983@users.noreply.github.com>
2017-12-25 14:04:08 -0500

    Add files via upload

--- variables.rb
+++ variables.rb
@@ -9,4 +9,17 @@ composer = 'Mozart'
 puts composer + ' was "da bomb" in his day.'
 composer = 'Beethoven'
 puts 'But I prefer ' + composer + ', personally.'
-my_own_var = 'just another' + 'string'
+puts ''
+my_own_var = 'just another' + ' string'
+puts my_own_var
+my_own_var = 5 * (1+2)
+puts my_own_var
+puts ''
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+puts ''
+var1 = 'eight'
+puts var1
+puts var2
